3d907fb1cd7c73589f041644712011c11ab86757
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-13 21:53:02 +0530

    custom form field

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,16 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -36,22 +27,11 @@ export const PatientForm = () => {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+      <section className="mb-12 space-y-4">
+        <h1 className="header">Hi there 👋🏼</h1>
+        <p className="text-dark-700">Schedule your appoinment</p>
+      </section>
+        
         <Button type="submit">Submit</Button>
       </form>
     </Form>
