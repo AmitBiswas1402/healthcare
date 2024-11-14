4cec488a3c53f18064e7767b9e0f3636a238e428
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-11-14 16:20:28 +0530

    custom props and rendering

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -8,23 +8,51 @@ import {
     FormMessage,
   } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Control } from 'react-hook-form';
+import { FormFieldType } from './forms/PatientForm';
 
-const CustomFormField = () => {
+interface CustomProps {
+  control: Control<any>,
+  fieldType: FormFieldType,
+  name: string,
+  label?: string,  
+  placeholder?: string,
+  iconSrc?: string,
+  iconAlt?: string,
+  disabled?: boolean,
+  dateFormat?: string,
+  showTimeSelection?: boolean,
+  children?: React.ReactNode,
+  renderSkeleton?: (field: any) => React.ReactNode,
+}
+
+const RenderField = ({field, props}: {field: any; props: CustomProps}) => {
+  return (
+    <Input
+      type='text'
+      placeholder='Your name'
+    />
+  )
+}
+
+const CustomFormField = (props: CustomProps) => {
+  const {control, fieldType, name, label} = props
+
   return (
     <div>
         <FormField
-          control={form.control}
-          name="username"
+          control={control}
+          name={name}
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+              {fieldType !== FormFieldType.CHECKBOX && label && (
+                <FormLabel>{label}</FormLabel>
+              )}
+
+              <RenderField field={field} props={props} />
+
+              <FormMessage className='shad-error' />
+
             </FormItem>
           )}
         />
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,6 +5,17 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
+import CustomFormField from "../CustomFormField";
+
+export enum FormFieldType {
+  INPUT="input",
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  SELECT = 'select',
+  DATE_PICKER = 'datePicker',
+  SKELETON = 'skeleton',
+}
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -28,10 +39,18 @@ export const PatientForm = () => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
       <section className="mb-12 space-y-4">
-        <h1 className="header">Hi there 👋🏼</h1>
+        <h1 className="header">Hi there👋🏼</h1>
         <p className="text-dark-700">Schedule your appoinment</p>
       </section>
-        
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control} 
+          name="name"
+          placeholder="Your name"
+          label="Name"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
